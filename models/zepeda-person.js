067bbf0ed2eb6c989cbf54b48c5d6879662abafe
Evelyn Zepeda <evaynnn@gmail.com>
2024-02-09 11:40:07 -0800

    Week 5

---
+++ models/zepeda-person.js
@@ -0,0 +1,34 @@
+/*
+============================================
+; Title:  zepeda-person.js
+; Author: Evelyn Zepeda
+; Date:   2/9/24
+; Description: People Mongoose model
+;===========================================
+*/
+
+const mongoose = require('mongoose');
+let Schema = mongoose.Schema;
+
+//Creating the roleSchema
+let roleSchema = new Schema({
+    text: {type: String}
+})
+
+//Creating the dependent Schema
+let dependentSchema = new Schema({
+    firstName: {type: String},
+    lastName: {type: String}
+})
+
+//Creating the person Schema
+let personSchema = new Schema({
+    firstName: {type: String},
+    lastName: {type: String},
+    roles: [roleSchema],
+    dependents: [dependentSchema],
+    birthDate: {type: String}
+})
+
+//Exporting the Schema
+module.exports = mongoose.model("Person", personSchema)
---
+++ routes/zepeda-person-routes.js
@@ -0,0 +1,9 @@
+/*
+============================================
+; Title:  zepeda-person-routes.js
+; Author: Evelyn Zepeda
+; Date:   2/9/24
+; Description: People Mongoose Routes
+;===========================================
+*/
+
